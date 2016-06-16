92fa248b0533c8effbd13b49cb93c187a86df7da
SofiaCortes <sofia_tes@hotmail.com>
2016-06-15 23:53:12 -0300

    Update calculo.js

--- js/calculo.js
+++ js/calculo.js
@@ -2,31 +2,30 @@
     function updateTextInput(val) {
         document.getElementById('textInput').value=val+"º";
     }
-    
+
+    function n1Update(n1) {
+        document.getElementById("n1Input").value = n1 ;
+    }
+
+    function n2Update(n2) {
+        document.getElementById("n2Input").value = n2 ;
+    }
+
 
     function myFunction() {
         var x = document.getElementById("mySelect1").value;
         var y = document.getElementById("mySelect2").value;
         var ang = document.getElementById("textInput").value;
-        var z = parseFloat(x)+parseFloat(y)+ parseInt(ang);
+        var z = (parseFloat(x)/parseFloat(y))* parseInt(ang);
+        z = z.toFixed(2);
         document.getElementById("Resultado").innerHTML = z+"º" ;
+
+        dibujaMedio();
+        dibujaRayo(document.getElementById("textInput").value, document.getElementById("textInput").value);
     }
 
 
-    function cambioColor1() {
-        if (document.getElementById("mySelect1").value = 1.330)
-        {
-            ctx.fillStyle="#ff2fff";
 
-        }
-    }
-
-    function cambioColor2() {
-        if (document.getElementById("mySelect1").value = 1.330)
-        {
-            ctx.fillStyle="#4785f4";
-        }
-    }
     
     
     
