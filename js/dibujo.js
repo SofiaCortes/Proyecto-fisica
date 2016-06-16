9db0e2173b9a57055f2827d9ad8e3210771ceedf
SofiaCortes <sofia_tes@hotmail.com>
2016-06-16 10:31:27 -0300

    Update dibujo.js

--- js/dibujo.js
+++ js/dibujo.js
@@ -7,7 +7,7 @@ function dibujaMedio() {
 
 
     var ctx = c.getContext("2d");
-    ctx.beginPath()
+    ctx.beginPath();
     if(document.getElementById("mySelect1").value==1)
         ctx.fillStyle = "#fffff1";
     else if(document.getElementById("mySelect1").value==1.330)
@@ -19,20 +19,20 @@ function dibujaMedio() {
     ctx.fillRect(0, 0, 550, 200);
 
     //m2
-    ctx.beginPath()
-    if(document.getElementById("mySelect1=2").value==1)
+    ctx.beginPath();
+    if(document.getElementById("mySelect2").value==1)
         ctx.fillStyle = "#fffff1";
-    else if(document.getElementById("mySelect1").value==1.330)
+    else if(document.getElementById("mySelect2").value==1.330)
         ctx.fillStyle = "#00f9ff";
-    else if(document.getElementById("mySelect1").value==1.47)
+    else if(document.getElementById("mySelect2").value==1.47)
         ctx.fillStyle = "#f6f66c";
-    else if(document.getElementById("mySelect1").value==1.492)
+    else if(document.getElementById("mySelect2").value==1.492)
         ctx.fillStyle = "#50b5d2";
     ctx.fillRect(0, 200, 550, 400);
 
 
     //Linea del medio
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.strokeStyle = '#000000';
     ctx.moveTo(0,200);
@@ -40,7 +40,7 @@ function dibujaMedio() {
     ctx.stroke();
 
     //Linea Normal
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.setLineDash([5, 10]);
     ctx.strokeStyle = '#000000';
@@ -50,23 +50,28 @@ function dibujaMedio() {
 }
 
 function dibujaRayo(n1, n2) {
+    var n1= document.getElementById(textInput);
+    var n2= document.getElementById(Resultado);
 
     //Rayo Incidente
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.moveTo(275, 200);
-    ctx.lineTo(parseInt(200/Math.tan(n1), 0);      // Ver como sería el angulo con n1 y n2
+    if (n1<54)
+        ctx.lineTo(parseInt(200*Math.tan(n1)), 0);
+    else
+        ctx.lineTo(0, parseInt(275/Math.tan(n1)));
     ctx.strokeStyle = '#663399';
     ctx.stroke();
 
 
     //Rayo Refractado
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.moveTo(275, 200);  // Ver como sería el angulo con n1 y n2
-    ctx.lineTo(parseInt(200*Math.tan(document.getElementById("Resultado").value)), 400);
+    ctx.lineTo(parseInt(200*Math.tan(n2)), 400);
     ctx.strokeStyle = '#663399';
     ctx.stroke();
 }
